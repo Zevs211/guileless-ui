81487191fe89fbf8a32c1cb7e372cb1cd2d7e43e
Zevs211 <fedoseeff1995@mail.ru>
2021-07-28 16:45:34 +0300

    replace: custom styles with tailwind classes; add: transition to dropdown popup; refactor: options computed to method;

--- src/components/VDropdown.vue
+++ src/components/VDropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative">
+  <div class="relative" v-click-outside="close">
     <div
       class="w-full cursor-pointer h-8 border-solid border-2 rounded-lg px-2 hover:bg-gray-200"
       @click="isOpen = !isOpen"
@@ -9,19 +9,21 @@
         <span>▼</span>
       </div>
     </div>
-    <div
-      class="absolute w-full top-10 border-solid border-2 rounded-lg"
-      v-if="isOpen"
-    >
-      <div v-for="(item, index) in options" :key="index">
-        <div
-          class="w-full h-8 cursor-pointer px-2 hover:bg-gray-200 "
-          @click="select(index)"
-        >
-          {{ item[itemValue] }}
+    <transition name="fade">
+      <div
+        class="absolute w-full top-10 border-solid border-2 rounded-lg bg-white"
+        v-if="isOpen"
+      >
+        <div v-for="(item, index) in savedOptions" :key="index">
+          <div
+            class="w-full h-8 cursor-pointer px-2 hover:bg-gray-200 "
+            @click="select(index)"
+          >
+            {{ item[itemValue] }}
+          </div>
         </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -46,19 +48,20 @@ export default {
     }
   },
   computed: {
-    options() {
-      this.savedOptions = this.items
-      if (typeof this.items[0] !== 'object') {
-        this.savedOptions = this.items.map(item => ({
-          [this.itemValue]: item,
-        }))
-      }
-      return this.savedOptions
-    },
     selectedValue() {
-      if (this.savedOptions.length === 0) return '5'
       return this.savedOptions[this.selectedIndex][this.itemValue]
     },
+  },
+  watch: {
+    items() {
+      this.processOptions()
+    },
+  },
+  created() {
+    this.processOptions()
+  },
+  mounted() {
+    this.select(0)
   },
   methods: {
     select(index) {
@@ -66,8 +69,30 @@ export default {
       this.$emit('on-select', this.items[index])
       this.isOpen = false
     },
+    close() {
+      this.isOpen = false
+    },
+    processOptions() {
+      this.savedOptions = this.items
+      if (typeof this.items === 'function') {
+        throw new Error('Options array elements cannot be a function')
+      }
+      if (typeof this.items[0] !== 'object') {
+        this.savedOptions = this.items.map(item => ({
+          [this.itemValue]: item,
+        }))
+      }
+    },
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.25s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
