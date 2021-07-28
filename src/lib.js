207f3a65af6fa2f23d1c7aebb416b995eb228f68
Zevs211 <fedoseeff1995@mail.ru>
2021-07-28 18:46:13 +0300

    fix: export lib components

--- src/lib.js
+++ src/lib.js
@@ -2,4 +2,4 @@ import VTable from '@/components/VTable.vue'
 import VDropdown from '@/components/VDropdown.vue'
 import VInput from '@/components/VInput.vue'
 
-export default { VTable, VDropdown, VInput }
+export { VTable, VDropdown, VInput }
